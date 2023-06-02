ea64125c88644494255f1db05d90012e428aed51
kevster01 <90598902+kevster01@users.noreply.github.com>
2023-06-01 21:45:23 -0700

    Add files via upload

---
+++ Topic 1: Two-Dimensional Arrays/ Assignment1/GUI Car Store/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopGui
+{
+    public class Car
+    {
+
+        //Car myNewSportsCar= new Car(2021, "Ford", "Mustang", "Red", 19500);
+
+        //Car genericCar = new Car();
+
+      public int Year { get; set; }
+      public string Make { get; set; }
+      public string Model { get; set; }
+      public string Color { get; set; }
+      public decimal Price { get; set; }
+        
+
+        
+
+      public Car(int year, string make, string model, string color, decimal price)
+      {
+            Year = year;
+            Make = make;
+            Model = model;
+            Color = color;
+            Price = price;
+      }
+
+      public Car()
+      {
+            Year = 0;
+            Make = "Nothing Yet";
+            Model = "Nothing Yet";
+            Color = "Nothing Yet";
+            Price = 0;      
+      }
+
+      public string Display
+      {
+            get
+            {
+                return string.Format("{0} {1} {2} {3} ${4}", Year, Make, Model, Color, Price);
+            }
+      }
+
+
+
+    }
+}
---
+++ Topic 1: Two-Dimensional Arrays/ Assignment1/GUI Car Store/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarShopGui
+{
+    public partial class Form1 : Form
+    {
+        Store store = new Store();
+
+        BindingSource carListBinding = new BindingSource();
+        BindingSource ShoppingListBinding = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetBindings();
+        }
+
+        private void SetBindings()
+        {
+            carListBinding.DataSource = store.CarList;
+            listBox1.DataSource = carListBinding;
+            listBox1.DisplayMember = "Display";
+            listBox1.ValueMember = "Display";
+
+            ShoppingListBinding.DataSource = store.ShoppingList;
+            listBox2.DataSource = ShoppingListBinding;
+            listBox2.DisplayMember = "Display";
+            listBox2.ValueMember = "Display";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Car newCar = new Car();
+                newCar.Year = int.Parse(textBox1.Text);
+                newCar.Make = textBox2.Text;
+                newCar.Model = textBox3.Text;
+                newCar.Color = textBox4.Text;
+                newCar.Price = decimal.Parse(textBox5.Text);
+
+                store.CarList.Add(newCar);
+
+                carListBinding.ResetBindings(false);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error has occured");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            store.ShoppingList.Add((Car)listBox1.SelectedItem);
+            ShoppingListBinding.ResetBindings(false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            decimal total = store.checkout();
+            label7.Text = total.ToString();
+
+        }
+    }
+}
