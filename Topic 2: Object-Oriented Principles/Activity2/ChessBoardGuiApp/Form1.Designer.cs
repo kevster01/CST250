448ec7a908286759d28cca2292a0c7b3e8584de2
kevster01 <90598902+kevster01@users.noreply.github.com>
2023-06-13 11:44:19 -0700

    Add files via upload

---
+++ Topic 2: Object-Oriented Principles/Activity2/ChessBoardGuiApp/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ChessBoardGUIApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(828, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a type of chess piece and then click on the grid. I will show you all lega" +
+    "l moves";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bishop",
+            "King ",
+            "Knight",
+            "Queen",
+            "Rook"});
+            this.comboBox1.Location = new System.Drawing.Point(915, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 33);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 1000);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 1123);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Topic 2: Object-Oriented Principles/Activity2/ChessBoardGuiApp/Form1.cs
@@ -0,0 +1,92 @@
+﻿using ChessBoardModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoardGUIApp
+{
+    public partial class Form1 : Form
+    {
+        static public Board myBoard = new Board(8);
+        public Button[,] btnGrid = new Button[myBoard.Size, myBoard.Size];
+
+        public Form1()
+        {
+            InitializeComponent();
+            populateGrid();
+        }
+
+        /// <summary>
+        /// Populates Grid
+        /// </summary>
+        public void populateGrid()
+        {
+            int buttonSize = panel1.Width / myBoard.Size;
+            panel1.Height = panel1.Width;
+
+            for (int r = 0; r < myBoard.Size; r++)
+            {
+                for (int c = 0; c < myBoard.Size; c++) 
+                {
+                    btnGrid[r, c] = new Button();
+                    btnGrid[r, c].Width = buttonSize;
+                    btnGrid[r, c].Height = buttonSize;
+
+                    btnGrid[r, c].Click += Grid_Button_Click;
+                    panel1.Controls.Add(btnGrid[r, c]);
+                    btnGrid[r, c].Location = new Point(buttonSize * r, buttonSize * c);
+                    btnGrid[r, c].Text = r.ToString() + "|" + c.ToString();
+                    btnGrid[r, c].Tag = r.ToString() + "|" + c.ToString();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates actions for grid selection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Grid_Button_Click(object sender, EventArgs e)
+        {
+            string[] strArr = (sender as Button).Tag.ToString().Split('|');
+            int r = int.Parse(strArr[0]);
+            int c = int.Parse(strArr[1]);
+
+            Cell currentCell = myBoard.theGrid[r, c];
+            myBoard.MarkNextLegalMoves(currentCell, "Knight");
+            updateButtonLabels();
+
+            for (int i = 0; i <myBoard.Size; i++)
+            {
+                for (int j = 0;  j <myBoard.Size; j++)
+                {
+                    btnGrid[i, j].BackColor = default(Color);
+                }
+            }
+
+            (sender as Button).BackColor = Color.Cornsilk;
+        }
+
+        /// <summary>
+        /// Update button labels 
+        /// </summary>
+        private void updateButtonLabels()
+        {
+            for (int r = 0; r < myBoard.Size; r++)
+            {
+                for (int c = 0; c < myBoard.Size; c++)
+                {
+                    btnGrid[r, c].Text = " ";
+                    if (myBoard.theGrid[r, c].CurrentlyOccupied) btnGrid[r, c].Text = "Knight ";
+                    if (myBoard.theGrid[r, c].LegalNextMove) btnGrid[r, c].Text = "Legal";
+                }
+            }
+        }
+    }
+}
